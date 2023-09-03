7512b5478937164bd5e1b8066b2b44da26e18e1e
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-03 17:28:28 +0300

    ejs integration with express

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 const mongoose = require("mongoose");
+const BlogPost = require("./models/BlogPost");
+
 mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
 
 app.use(express.static("public"));
@@ -15,8 +16,15 @@ app.listen(4000, () => {
   console.log("App listening on port 4000");
 });
 
-app.get("/", (req, res) => {
-  res.render("index");
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+app.get("/", async (req, res) => {
+  const blogposts = await BlogPost.find({});
+  res.render("index", {
+    blogposts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -35,7 +43,17 @@ app.get("/posts/new", (req, res) => {
   res.render("create");
 });
 
-app.post("/posts/store", (req, res) => {
-  console.log(req.body);
+// app.post("/posts/store", (req, res) => {
+//   BlogPost.create(req.body)
+//     .then(() => {
+//       res.redirect("/");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+
+app.post("/posts/store", async (req, res) => {
+  await BlogPost.create(req.body);
   res.redirect("/");
 });
