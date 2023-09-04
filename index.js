a5923d073a9ff0a52e6c85aa58def167247214f0
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-04 17:50:48 +0300

    blog site entegration with mvc

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+//const path = require("path");
 const app = new express();
 const ejs = require("ejs");
 app.set("view engine", "ejs");
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
-const BlogPost = require("./models/BlogPost");
+//const BlogPost = require("./models/BlogPost");
 const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 
@@ -18,35 +18,53 @@ app.listen(4000, () => {
   console.log("App listening on port 4000");
 });
 
+const validateMiddleware = require("./middleware/validationMiddleware.js");
+//validationMiddleware.js
+// const validateMiddleWare = (req, res, next) => {
+//   console.log(req);
+//   if (req.files == null || req.body.title == null || req.body.body == null) {
+//     return res.redirect("/posts/new");
+//   }
+//   next();
+// };
+app.use("/posts/store", validateMiddleware);
+
 // app.get("/", (req, res) => {
 //   res.render("index");
 // });
 
-app.get("/", async (req, res) => {
-  const blogposts = await BlogPost.find({});
-  res.render("index", {
-    blogposts,
-  });
-});
+const homeController = require("./controllers/home");
+app.get("/", homeController);
+//home.js
+// app.get("/", async (req, res) => {
+//   const blogposts = await BlogPost.find({});
+//   res.render("index", {
+//     blogposts,
+//   });
+// });
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+//Deleted
+// app.get("/about", (req, res) => {
+//   res.render("about");
+// });
 
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+//Deleted
+// app.get("/contact", (req, res) => {
+//   res.render("contact");
+// });
 
-app.get("/post/:id", async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id);
-  res.render("post", {
-    blogpost,
-  });
-});
+const getPostController = require("./controllers/getPost");
+app.get("/post/:id", getPostController);
+//getpost.js
+// app.get("/post/:id", async (req, res) => {
+//   const blogpost = await BlogPost.findById(req.params.id);
+//   res.render("post", {
+//     blogpost,
+//   });
+// });
 
-app.get("/posts/new", (req, res) => {
-  res.render("create");
-});
+const newPostController = require("./controllers/newPost");
+app.get("/posts/new", newPostController);
 
 // app.post("/posts/store", (req, res) => {
 //   BlogPost.create(req.body)
@@ -58,13 +76,19 @@ app.get("/posts/new", (req, res) => {
 //     });
 // });
 
-app.post("/posts/store", async (req, res) => {
-  let image = req.files.image;
-  image.mv(
-    path.resolve(__dirname, "public/assets/img", image.name),
-    async (error) => {
-      await BlogPost.create(req.body);
-      res.redirect("/");
-    }
-  );
-});
+const storePostController = require("./controllers/storePost");
+app.post("/posts/store", storePostController);
+//storePost.js
+// app.post("/posts/store", async (req, res) => {
+//   let image = req.files.image;
+//   image.mv(
+//     path.resolve(__dirname, "public/assets/img", image.name),
+//     async (error) => {
+//       await BlogPost.create({
+//         ...req.body,
+//         image: "/assets/img/" + image.name,
+//       });
+//       res.redirect("/");
+//     }
+//   );
+// });
