b0358ccdc7c3a09945ddacd232de43801488f3f0
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-04 12:38:43 +0300

    blog get data from database

--- index.js
+++ index.js
@@ -35,8 +35,11 @@ app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.get("/post", (req, res) => {
-  res.render("post");
+app.get("/post/:id", async (req, res) => {
+  const blogpost = await BlogPost.findById(req.params.id);
+  res.render("post", {
+    blogpost,
+  });
 });
 
 app.get("/posts/new", (req, res) => {
