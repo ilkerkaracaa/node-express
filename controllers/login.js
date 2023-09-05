6094eedadb4231606efa58bf7af632887ae366a4
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-05 18:42:43 +0300

    login işleminde alanlari doğrulama

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,13 @@
 module.exports = (req, res) => {
-  res.render("login");
+  let username = "";
+  let password = "";
+  const data = req.flash("data")[0];
+  if (typeof data != "undefined") {
+    (username = data.username), (password = data.password);
+  }
+  res.render("login", {
+    errors: req.flash("error"),
+    username: username,
+    password: password,
+  });
 };
--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -2,6 +2,16 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 module.exports = (req, res) => {
   const { username, password } = req.body;
+  console.log(req.body);
+  if (username === "" && password === "") {
+    error("all fields are required");
+  }
+  if (username === "") {
+    error("username is required");
+  }
+  if (password === "") {
+    error("password is required");
+  }
   User.findOne({ username: username })
     .then((user) => {
       if (user) {
@@ -12,12 +22,21 @@ module.exports = (req, res) => {
             req.session.userId = user._id;
             res.redirect("/");
           } else {
+            const error = new Array("password is incorrect");
+            req.flash("error", error);
+            req.flash("data", req.body);
             res.redirect("/auth/login");
           }
         });
       }
     })
     .catch((error) => {
-      res.redirect("/auth/login");
+      error("username is incorrect");
     });
+  function error(arr) {
+    const error = new Array(arr);
+    req.flash("error", error);
+    req.flash("data", req.body);
+    res.redirect("/auth/login");
+  }
 };
